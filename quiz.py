e9f75ad741a29fb8115310b67ba5d29fb388bb04
Gabriel da Silva Pereira <gsp@cyon.ch>
2023-03-13 14:00:53 +0100

    Improve GUI

--- quiz.py
+++ quiz.py
@@ -19,14 +19,16 @@ def ask_question():
     """
     Creates the questions and outputs them randomly
     """
-    global current_question
+    global current_question, var, option_buttons
     while True:
         current_question = random.choice(list(questions.items()))
         if current_question not in answered_questions:
             break
     question_label.config(text=current_question[0])
     for i in range(len(current_question[1]["options"])):
-        option_buttons[i].config(text=current_question[1]["options"][i], font=("Monospace", 40), height=2, width=30, bg="white")
+        option_buttons[i].config(text=current_question[1]["options"][i], font=("Monospace", 40), height=2, width=len(current_question[1]["options"][i]) + 20, bg="white", command=lambda idx=i: set_answer_bg(idx))
+        option_buttons[i].pack(side="top", fill="x", padx=10, pady=10)
+        option_buttons[i].config(anchor="center", variable=var, value=i)
     answered_questions.append(current_question)
 
 
@@ -34,24 +36,29 @@ def set_answer_bg(button_idx):
     """
     Sets the background of the selected answer to orange and the background of the other options to white.
     """
+    global option_buttons
+    var.set(button_idx)
     for i, button in enumerate(option_buttons):
         if i == button_idx:
-            button.config(bg="orange")
+            button.config(bg="orange", fg="white")
         else:
-            button.config(bg="white")
+            button.config(bg="white", fg="black")
+        button.config(width=len(current_question[1]["options"][i]) + 20, anchor="center")
 
 
 def submit():
     """
     Creates the Sumbit Button and adds to the Score if question is right. If it was the last Question it calls the end_quiz function.
     """
-    global score
+    global score, var, answered_questions, option_buttons
     if current_question[1]["answer"] == current_question[1]["options"][var.get()]:
         score += 1
     score_label.config(text="Score: " + str(score))
     if len(answered_questions) == len(questions):
         end_quiz()
     else:
+        for button in option_buttons:
+            button.pack_forget()
         ask_question()
 
 
@@ -69,6 +76,7 @@ def end_quiz():
 
 # create the GUI
 root = tk.Tk()
+var = tk.IntVar(value=-1)
 root.title("Quiz")
 root.geometry("1000x800")
 
@@ -77,26 +85,31 @@ root.configure(bg="white")
 root.option_add("*Font", font.Font(family="Monospace", size=60))
 
 # create the widgets
+title_frame = tk.Frame(root, bg="white")
+title_frame.pack(side="top", pady=10)
+
+score_label = tk.Label(title_frame, text="Score: 0", fg="black", bg="white", anchor="center", font=("Monospace", 40))
+score_label.pack(side="left", padx=50, fill="x")
+
+question_label = tk.Label(title_frame, text="", fg="black", bg="white", font=("Monospace", 60))
+question_label.pack(side="left", expand=True, fill="x")
+
+submit_button = tk.Button(title_frame, text="Bestätigen", command=submit, fg="white", bg="#00cc00", padx=60, pady=20, font=("Monospace", 40))
+submit_button.pack(side="right", padx=50)
+
 question_frame = tk.Frame(root, bg="white")
 question_frame.pack(pady=40)
-question_label = tk.Label(question_frame, text="", fg="black", bg="white", font=("Monospace", 60))
-question_label.pack()
 
-var = tk.IntVar()
 option_frame = tk.Frame(root, bg="white")
 option_frame.pack()
+
 option_buttons = []
+max_answer_len = 0
 for i in range(4):
-    button = tk.Radiobutton(option_frame, text="", variable=var, value=i, fg="black", bg="white", pady=20, indicatoron=0,
+    button = tk.Radiobutton(option_frame, text="", variable=var, value=i, fg="black", bg="white", pady=10, padx=20, indicatoron=0,
                             selectcolor="orange")
-    button.pack(pady=20)
+    button.pack(side="top", fill="x", padx=10, pady=10)
     option_buttons.append(button)
-
-submit_button = tk.Button(root, text="Bestätigen", command=submit, fg="white", bg="#00cc00", padx=60, pady=20, font=("Monospace", 40))
-submit_button.pack(pady=40)
-
-score_label = tk.Label(root, text="Score: 0", fg="black", bg="white", pady=40, font=("Monospace", 40))
-score_label.pack(side="bottom")
 
 # ask the first question
 ask_question()
--- quiz_questions.py
+++ quiz_questions.py
@@ -1,23 +1,257 @@
+"""
+These are the questions to the quiz.
+"""
 questions = {
-    "Was ist die Hauptstadt von Deutschland?": {
-        "options": ["Berlin",
-                    "München",
-                    "Hamburg",
-                    "Köln"],
-        "answer": "Berlin"
+    "Was ist ARP ausgeschrieben?": {
+        "options": ["Address Resolution Protocol",
+                    "Analytical Research Project",
+                    "American Registry for Internet Numbers",
+                    "Apache Resource Processor"],
+        "answer": "Address Resolution Protocol"
     },
-    "Was ist die höchste Zahl?": {
-        "options": ["100",
-                    "1.000",
-                    "10.000",
-                    "100.000"],
-        "answer": "100.000"
+    "Was macht ARP?": {
+        "options": ["Um die MAC-Adresse eines Netzwerkgeräts zu ermitteln",
+                    "Zur Übertragung von Daten zwischen verschiedenen Netzwerken",
+                    "Zum Aufbau einer sicheren Verbindung zwischen einem Client und einem Server",
+                    "Zur Konfiguration von Firewalls auf Netzwerkgeräten"],
+        "answer": "Um die MAC-Adresse eines Netzwerkgeräts zu ermitteln"
     },
-    "Wer hat die Relativitätstheorie entdeckt?": {
-        "options": ["Isaac Newton",
-                    "Albert Einstein",
-                    "Galileo Galilei",
-                    "Stephen Hawking"],
-        "answer": "Albert Einstein"
+    "Was ist ASCII ausgeschrieben?": {
+        "options": ["Amendment Stadard Code for Interchange Information",
+                    "American Stadard Code for Informational Intercharge",
+                    "American Secret Code for Information Interchange",
+                    "American Stadard Code for Information Interchange"],
+        "answer": "American Stadard Code for Information Interchange"
+    },
+    "Was macht ASCII?": {
+        "options": ["Eine Programmiersprache, die in der Entwicklung von Webanwendungen verwendet wird.",
+                    "Eine Art von Datenkompression, die verwendet wird, um große Dateien zu verkleinern.",
+                    "Eine standardisierte Codierung von Zeichen, die zur Darstellung von Text in Computern und anderen Geräten verwendet wird.",
+                    "Eine Art von Angriff auf Computersysteme, bei dem ein Angreifer versucht, einen Pufferüberlauf auszunutzen."],
+        "answer": "Eine standardisierte Codierung von Zeichen, die zur Darstellung von Text in Computern und anderen Geräten verwendet wird."
+    },
+    "Was ist CARP ausgeschrieben?": {
+        "options": ["Computer Address Redundancy Protocol",
+                    "Common Address Resolution Protocol",
+                    "Common Address Redundancy Protocol",
+                    "Computing Arbitrary Redundancy Protocol"],
+        "answer": "Common Address Redundancy Protocol"
+    },
+    "Was macht CARP?": {
+        "options": ["Ein Netzwerkprotokoll zur Routenauswahl",
+                    "Ein Netzwerk-Protokoll zur Übertragung von Audiodaten",
+                    "Eine Programmiersprache für Web-Entwicklung",
+                    "Ein Verschlüsselungs-Algorithmus für Datenübertragung"],
+        "answer": "Ein Netzwerkprotokoll zur Routenauswahl"
+    },
+    "Was ist CDN ausgeschrieben?": {
+        "options": ["Content Delivery Network",
+                    "Convertion Delivery Network",
+                    "Convertion Destination Network",
+                    "Content Drugs Network"],
+        "answer": "Content Delivery Network"
+    },
+    "Was macht CDN?": {
+        "options": ["Ein Werkzeug zur Dateiverwaltung",
+                    "Ein Netzwerk von Servern zur Bereitstellung von Inhalten",
+                    "Ein Protokoll zur Datenübertragung",
+                    "Eine Bibliothek zur Verarbeitung von Bildern"],
+        "answer": "Ein Netzwerk von Servern zur Bereitstellung von Inhalten."
+    },
+    "Was ist daemon ausgeschrieben?": {
+        "options": ["Disk and Extenstions Managment",
+                    "Data and Execution Managment",
+                    "Disk and Execution Monitoring",
+                    "Data and Excel Monitoring"],
+        "answer": "Disk and Execution Monitoring"
+    },
+    "Was macht ein daemon": {
+        "options": ["Ist falsch geschrieben, eigentlich sollte es 'Dämon' heissen.",
+                    "Es ist eine Firewall-Software",
+                    "Es ist ein Programm welches Dateiübertragung ermöglicht",
+                    "Ist ein Programm welcher kontinuierlich als Hintergrundprozess lauft, ausserdem beantowrtet es Dienstanfragen."],
+        "answer": "Ist ein Programm welcher kontinuierlich als Hintergrundprozess lauft, ausserdem beantowrtet es Dienstanfragen."
+    },
+    "Was ist DHCP ausgeschrieben?": {
+        "options": ["Dynamic Host Controll Protocol",
+                    "Dynamic Host Configuration Protocol",
+                    "Dynamic Horst Configuration Protocol",
+                    "Delivery Host Conntroll Protocol"],
+        "answer": "Dynamic Host Configuration Protocol"
+    },
+    "Was macht DHCP?": {
+        "options": ["Wird verwendet um IP-Adressen und Netzwerkkonfigurationen automatisch an Geräte zu vergeben.",
+                    "Wird verwendet um Datenbanken auf Hosts zu konfigurieren.",
+                    "Wird verwendet um direkte Verbindungen zwischen Hosts herzustellen.",
+                    "Wird verwendet um digitale Zertifikate auf Hosts zu übertragen."],
+        "answer": "Wird verwendet um IP-Adressen und Netzwerkkonfigurationen automatisch an Geräte zu vergeben."
+    },
+    "Was ist DNS ausgeschrieben?": {
+        "options": ["Domain Name Server",
+                    "Dynamic Network System",
+                    "Data Network System",
+                    "Domain Name System"],
+        "answer": "Domain Name System"
+    },
+    "Was macht DNS?": {
+        "options": ["Es ermöglicht die Erstellung von Netzwerk-Protokollen.",
+                    "Es stellt sicher, dass E-Mails erfolgreich zugestellt werden.",
+                    "Es übersetzt Domainnamen in IP-Adressen.",
+                    "Es dient als Firewall und schützt das Netzwerk vor Angriffen."],
+        "answer": "Es übersetzt Domainnamen in IP-Adressen."
+    },
+    "Was ist FOSS ausgeschrieben?": {
+        "options": ["Free Online Software System",
+                    "Fully Operational System Software",
+                    "Free Open Source Software",
+                    "Frequently Overhauled System Software"],
+        "answer": "Free Open Source Software"
+    },
+    "Was ist FOSS?": {
+        "options": ["Eine spezielle Art von Betriebssystem für Mobiltelefone.",
+                    "Eine Methode zur Verschlüsselung von Daten auf einem Server.",
+                    "Eine Abkürzung für 'Free and Open-Source Software'.",
+                    "Ein Werkzeug zur Überwachung von Netzwerkverbindungen."],
+        "answer": "Eine Abkürzung für 'Free and Open-Source Software'."
+    },
+    "Was ist FQDN ausgeschrieben?": {
+        "options": ["Fully Qualified Domain Name",
+                    "Fast Query Distribution Network",
+                    "Free Quality Download Network",
+                    "File Query Distribution Node"],
+        "answer": "Fully Qualified Domain Name"
+    },
+    "Was ist ein FQDN?": {
+        "options": ["Ein vollständig qualifizierter Dateiname.",
+                    "Ein vollständig qualifizierter Domainname.",
+                    "Ein vollständig qualifizierter Prozessname.",
+                    "Ein vollständig qualifizierter Ordnername."],
+        "answer": "Ein vollständig qualifizierter Domainname."
+    },
+    "Was ist FS ausgeschrieben?": {
+        "options": ["Frisch gestrichen",
+                    "File System",
+                    "Fuzzy Search",
+                    "Falsch geschrieben"],
+        "answer": "File System"
+    },
+    "Was ist ein FS?": {
+        "options": ["Eine Sammlung von Musik-Dateien auf einem Computer.",
+                    "Ein Tool zum Schutz von sensiblen Daten auf einem Computer.",
+                    "Ein Tool zum Erstellen von Backups auf einem Computer.",
+                    "Eine Sammlung von Datensätzen auf einem Computerspeichermedium."],
+        "answer": "Eine Sammlung von Datensätzen auf einem Computerspeichermedium."
+    },
+    "Was ist FTP ausgeschrieben?": {
+        "options": ["File Transfer Protocol",
+                    "Fast Transmission Platform",
+                    "File Transfer Program",
+                    "Fast Text Processing"],
+        "answer": "File Transfer Protocol"
+    },
+    "Was ist FTP?": {
+        "options": ["Eine Software zum Entpacken von Dateien.",
+                    "Ein Protokoll zur Übertragung von Dateien zwischen Computern.",
+                    "Eine Software zum Synchronisieren von Dateien zwischen Computern.",
+                    "Eine Software zum Suchen von Dateien auf einem Computer."],
+        "answer": "Ein Protokoll zur Übertragung von Dateien zwischen Computern."
+    },
+    "Was ist GCC ausgeschrieben?": {
+        "options": ["Gnu Compiler Collection",
+                    "Graphical Command Center",
+                    "Global Caching Configuration",
+                    "General Command Counter"],
+        "answer": "Gnu Compiler Collection"
+    },
+    "Was ist GCC?": {
+        "options": ["Eine Sammlung von Computer-Tools zur Dateiverwaltung.",
+                    "Eine Sammlung von Computer-Tools zur Systemadministration.",
+                    "Eine Sammlung von Computer-Spielen.",
+                    "Eine Sammlung von Compiler-Tools zur Softwareentwicklung."],
+        "answer": "Eine Sammlung von Compiler-Tools zur Softwareentwicklung."
+    },
+    "Was ist GID ausgeschrieben?": {
+        "options": ["Group Identification",
+                    "Group Identifier",
+                    "Global Identifier",
+                    "Global Identification"],
+        "answer": "Group Identifier"
+    },
+    "Was ist GID?": {
+        "options": ["Ein Protokoll zur Vernetzung von Geräten",
+                    "Ein Dateisystem",
+                    "Eine Identifikationsnummer für Gruppen in einem Unix-basierten Betriebssystem",
+                    "Ein Verschlüsselungsalgorithmus"],
+        "answer": "Eine Identifikationsnummer für Gruppen in einem Unix-basierten Betriebssystem"
+    },
+    "Was ist GNU ausgeschrieben?": {
+        "options": ["Global Network Utility",
+                    "GNU's Not Unix",
+                    "General Network Utility",
+                    "Group Network Utility"],
+        "answer": "GNU's Not Unix"
+    },
+    "Was ist GNU?": {
+        "options": ["Eine Software für die Erstellung von Bildern und Grafiken",
+                    "Eine Suchmaschine für wissenschaftliche Veröffentlichungen",
+                    "Eine Sammlung von Betriebssystemkomponenten",
+                    "Eine Programmiersprache für maschinennahe Anwendungen"],
+        "answer": "Eine Sammlung von Betriebssystemkomponenten"
+    },
+    "Was ist GRUB ausgeschrieben?": {
+        "options": ["Grand Unified Bootloader",
+                    "Graphical User Bootloader",
+                    "Generic Universal Bootloader",
+                    "Global Unified Bootloader"],
+        "answer": "Grand Unified Bootloader"
+    },
+    "Was ist GRUB?": {
+        "options": ["Zur Ausführung von Java-Programmen auf einem Server.",
+                    "Zum Anzeigen von System- und Anwendungslogs.",
+                    "Zur Verwaltung von Netzwerkverbindungen auf einem Linux-System.",
+                    "Zum Laden und Starten von Betriebssystemen auf einem Computer."],
+        "answer": "Zum Laden und Starten von Betriebssystemen auf einem Computer."
+    },
+    "Was ist HFS ausgeschrieben?": {
+        "options": ["Hierarchical File System",
+                    "High-speed File System",
+                    "Hyper File System",
+                    "Host File System"],
+        "answer": "Hierarchical File System"
+    },
+    "Was ist HFS?": {
+        "options": ["Ein Verschlüsselungsalgorithmus für Netzwerksicherheit.",
+                    "Ein Dateisystem, das von Apple-Computern genutzt wird.",
+                    "Ein Netzwerkprotokoll für die Übertragung von Audio- und Video-Streaming.",
+                    "Eine Cloud-Plattform für die Speicherung und den Austausch von Dateien."],
+        "answer": "Ein Dateisystem, das von Apple-Computern genutzt wird."
+    },
+    "Was ist HSRP ausgeschrieben?": {
+        "options": ["Hot Standby Router Protocol",
+                    "Host Standby Router Protocol",
+                    "High-speed Router Protocol",
+                    "Hyper Standby Router Protocol"],
+        "answer": "Hot Standby Router Protocol"
+    },
+    "Was ist HSRP und wofür wird es genutzt?": {
+        "options": ["Ein Netzwerkprotokoll zur Routenauswahl",
+                    "Eine Sprache zur Erstellung von Webanwendungen",
+                    "Ein Verschlüsselungsstandard für Netzwerkdaten",
+                    "Eine Konfigurationsoption für Firewalls"],
+        "answer": "Ein Netzwerkprotokoll zur Routenauswahl"
+    },
+    "Was ist HTML ausgeschrieben?": {
+        "options": ["Hyperlinks and Text Markup Language",
+                    "Hyper Text Makeup Language",
+                    "Hyperlinks Text Management Language",
+                    "High-speed Text Markup Language"],
+        "answer": "Hyperlinks and Text Markup Language"
+    },
+    "Was ist HTML?": {
+        "options": ["HTML ist ein Betriebssystem für Computer.",
+                    "HTML ist eine Programmiersprache zur Entwicklung von Anwendungen.",
+                    "HTML ist ein Protokoll zur Datenübertragung im Internet.",
+                    "HTML ist eine Auszeichnungssprache zur Strukturierung von Webseiten."],
+        "answer": "HTML ist eine Auszeichnungssprache zur Strukturierung von Webseiten."
     }
 }
